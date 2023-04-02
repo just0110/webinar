d0947d1fed88a1abdca2c76c457e63d8324fc6b7
just0110 <hanna.likhtman@globallogic.com>
2023-04-02 16:10:23 +0300

    feat(login): create login form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import LoginForm from "./login/LoginForm";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Webinar from GlobalLogic!</h1>)
+root.render(<LoginForm />)
